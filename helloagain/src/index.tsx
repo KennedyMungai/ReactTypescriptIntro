34de4cd31abe6bbbb5687b6dff7f57dd2a509e8a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-07-05 09:40:10 +0300

    Adjustments to the basic react component

--- helloagain/src/index.tsx
+++ helloagain/src/index.tsx
@@ -1,12 +1,11 @@
 import ReactDOM from 'react-dom';
 
 const App = () => {
-    return <div className="">
-        <h1>Hi there!</h1>
-    </div>
-}
+    return(
+        <div className="Greeting">
+            <h1>Hi there!</h1>
+        </div>
+    );
+};
 
-ReactDOM.render(
-    <App />,
-    document.querySelector('#root')
-);
+ReactDOM.render(<App />,document.querySelector('#root'));
