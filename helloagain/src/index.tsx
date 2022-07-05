5df309351d51fa14d15ba940eba00297d1ac1c30
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-07-05 09:36:46 +0300

    Basic Hello World type react component

---
+++ helloagain/src/index.tsx
@@ -0,0 +1,12 @@
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return <div className="">
+        <h1>Hi there!</h1>
+    </div>
+}
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+);
