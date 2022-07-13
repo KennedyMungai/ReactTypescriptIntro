069dc91f83b4571771bc5911990bbaa9208abede
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-07-13 12:52:47 +0300

    Added a declaration for onClick

--- helloreact/src/Child.tsx
+++ helloreact/src/Child.tsx
@@ -1,15 +1,16 @@
 interface ChildProps {
     color: string;
+    onClick: () => void;
 }
 
-export const Child = ({ color }: ChildProps) => {
+export const Child = ({ color, onClick }: ChildProps) => {
     return <div>
         {color}
         <button onClick={onClick}>Click Me!!!</button>
         </div>
 };
 
-export const ChildAsFC: React.FC<ChildProps> = ({color}) => {
+export const ChildAsFC: React.FC<ChildProps> = ({color, onClick}) => {
     return <div>
         {color}
         <button onClick={onClick}>Click Me</button>
