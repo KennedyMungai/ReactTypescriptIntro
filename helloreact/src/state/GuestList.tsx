df84cc370e22246522368b71e4ca1c5685bd5d57
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-07-13 14:05:02 +0300

    Added state name

--- helloreact/src/state/GuestList.tsx
+++ helloreact/src/state/GuestList.tsx
@@ -1,6 +1,15 @@
+import { useState } from "react";
+
 const GuestList: React.FC = () => {
+    const[name, setName] = useState('');
+
     return(
-        <div>Guest List</div>
+        <div>
+            <h3>Guest List</h3>
+
+            <input value={name} onChange={(e) => setName(e.target.value)} />
+            <button>Add Guest</button>
+        </div>
     );
 };
 
