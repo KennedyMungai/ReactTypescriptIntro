9dd890c154b330c82acec21156b32a2d7b4e566b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-07-13 12:51:08 +0300

    Added buttons to the components

--- helloreact/src/Child.tsx
+++ helloreact/src/Child.tsx
@@ -3,11 +3,17 @@ interface ChildProps {
 }
 
 export const Child = ({ color }: ChildProps) => {
-    return <div>{color}</div>
+    return <div>
+        {color}
+        <button onClick={onClick}>Click Me!!!</button>
+        </div>
 };
 
 export const ChildAsFC: React.FC<ChildProps> = ({color}) => {
-    return <div>{color}</div>;
+    return <div>
+        {color}
+        <button onClick={onClick}>Click Me</button>
+        </div>;
 };
 
 ChildAsFC.displayName
