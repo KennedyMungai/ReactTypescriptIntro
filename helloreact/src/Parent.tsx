9af9439849c98497414304619c15499648064519
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-07-13 12:54:17 +0300

    Added onclick functionality

--- helloreact/src/Parent.tsx
+++ helloreact/src/Parent.tsx
@@ -1,7 +1,7 @@
 import {Child} from './Child';
 
 const Parent = () => {
-    return <Child color='Red' />
+    return <Child color='Red' onClick={() => console.log('Clicked')}/>
 };
 
 export default Parent;
