362449271d93450945551b7cfd2ebf3378386d1a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-07-13 14:38:24 +0300

    Added an array of users

---
+++ helloreact/src/state/UserSearch.tsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+const Users = [
+    {name: "Sarah", age:20},
+    {name: "Alex", age:20},
+    {name: "Michael", age:20}
+];
+
+
+const UserSearch:React.FC = () => {
+    return(
+        <div>
+            User Search
+        </div>
+    );
+};
+
+export default UserSearch;
