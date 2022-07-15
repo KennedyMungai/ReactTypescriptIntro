a6aeb1ac3764cf0fb26cbb8d6205771876876cd8
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-07-15 15:31:16 +0300

    Created a switch statement to handle the states

--- redux-ts/src/state/reducers/repositoriesReducer.ts
+++ redux-ts/src/state/reducers/repositoriesReducer.ts
@@ -5,7 +5,16 @@ interface RepositoriesState {
 }
 
 const reducer = (state: RepositoriesState, action: any) => {
+    switch (action.type) {
+        case 'search_repositories':
+            return {loading: true, error: null, data: []}
+        case 'search_repositories_success':
 
+        case 'search_repositories_error':
+            
+        default:
+            return state;
+    }
 };
 
 export default reducer;
