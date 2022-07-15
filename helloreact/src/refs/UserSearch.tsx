96aedeee0a863a5e8c4a3a5dbd3d5e8dd03b868b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-07-15 14:27:07 +0300

    inputRef initially assigned to null

--- helloreact/src/refs/UserSearch.tsx
+++ helloreact/src/refs/UserSearch.tsx
@@ -8,7 +8,7 @@ const Users = [
 
 
 const UserSearch:React.FC = () => {
-    const inputRef = useRef<HTMLInputElement | null>();
+    const inputRef = useRef<HTMLInputElement | null>(null);
     const [name, setName] = useState('');
     const [user, setUser] = useState<{name: string, age: number} | undefined>();
 
