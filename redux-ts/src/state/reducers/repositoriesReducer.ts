533107220377065c08759ebbcaee6042339b1476
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-07-20 14:15:37 +0300

    Created an enum for the action types called ActionType

--- redux-ts/src/state/reducers/repositoriesReducer.ts
+++ redux-ts/src/state/reducers/repositoriesReducer.ts
@@ -20,6 +20,12 @@ interface SearchRepositoriesErrorAction {
 
 type Action = SearchRepositoriesAction | SearchRepositoriesSuccessAction | SearchRepositoriesErrorAction;
 
+enum ActionType {
+    SEARCH_REPOSITORIES = 'search_repositories',
+    SEARCH_REPOSITORIES_SUCCESS = 'search_repositories_success',
+    SEARCH_REPOSITORIES_ERROR = 'search_repositories_error'
+}
+
 const reducer = (state: RepositoriesState, action: Action): RepositoriesState => {
     switch (action.type) {
         case 'search_repositories':
