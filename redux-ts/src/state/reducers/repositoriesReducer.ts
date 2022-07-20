2a1fb12f7eef08da7021e3072cea309c70519cfe
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-07-20 14:11:42 +0300

    Added the Action types to one Action type

--- redux-ts/src/state/reducers/repositoriesReducer.ts
+++ redux-ts/src/state/reducers/repositoriesReducer.ts
@@ -18,7 +18,9 @@ interface SearchRepositoriesErrorAction {
     payload: string;
 }
 
-const reducer = (state: RepositoriesState, action: SearchRepositoriesAction | SearchRepositoriesSuccessAction | SearchRepositoriesErrorAction): RepositoriesState => {
+type Action = SearchRepositoriesAction | SearchRepositoriesSuccessAction | SearchRepositoriesErrorAction;
+
+const reducer = (state: RepositoriesState, action: Action): RepositoriesState => {
     switch (action.type) {
         case 'search_repositories':
             return {loading: true, error: null, data: []}
