72f8745e283bd8747fec29969a53abde318e5bcb
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-07-20 14:53:13 +0300

    Added code for the npm repository search

--- redux-ts/src/state/action-creators/index.ts
+++ redux-ts/src/state/action-creators/index.ts
@@ -14,6 +14,16 @@ const searchRepositories = (term: string) => {
                     text: term
                 }
             });
+
+            const names = data.objects.map((result: any) => {
+                return result.package.name
+            });
+
+            dispatch({
+                type: ActionType.SEARCH_REPOSITORIES_SUCCESS, 
+                payload: names
+            });
+
         } catch (err) {
             dispatch({
                 type: ActionType.SEARCH_REPOSITORIES_ERROR, 
