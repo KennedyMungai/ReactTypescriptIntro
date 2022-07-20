0f9ad54be2897f78fd1c51c4e70d5064d7ae1fad
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-07-20 14:27:46 +0300

    Moved the enum of types to a new file

--- redux-ts/src/state/reducers/repositoriesReducer.ts
+++ redux-ts/src/state/reducers/repositoriesReducer.ts
@@ -1,10 +1,4 @@
 
-enum ActionType {
-    SEARCH_REPOSITORIES = 'search_repositories',
-    SEARCH_REPOSITORIES_SUCCESS = 'search_repositories_success',
-    SEARCH_REPOSITORIES_ERROR = 'search_repositories_error'
-}
-
 const reducer = (state: RepositoriesState, action: Action): RepositoriesState => {
     switch (action.type) {
         case ActionType.SEARCH_REPOSITORIES:
