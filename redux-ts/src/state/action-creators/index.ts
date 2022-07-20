e91987dc4a8d460adce296450f15dd2dd77b59f4
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-07-20 15:01:44 +0300

    Made dispatch statically typed with Dispatch<Action>

--- redux-ts/src/state/action-creators/index.ts
+++ redux-ts/src/state/action-creators/index.ts
@@ -4,7 +4,7 @@ import { ActionType } from "../action-types";
 import { Action } from "../actions";
 
 export const searchRepositories = (term: string) => {
-    return async (dispatch: any) => {
+    return async (dispatch: Dispatch<Action>) => {
         dispatch({
             type: ActionType.SEARCH_REPOSITORIES
         });
