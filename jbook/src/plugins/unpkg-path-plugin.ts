d496741f6fe5d0f9135be61d4b0b81df65498ec1
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-07-26 12:39:01 +0300

    Added the namespace 'a'

--- jbook/src/plugins/unpkg-path-plugin.ts
+++ jbook/src/plugins/unpkg-path-plugin.ts
@@ -11,7 +11,7 @@ export const unpkgPathPlugin = () => {
           return { path: args.path, namespace: 'a' };
         } else if (args.path === 'tiny-test-pkg')
         {
-          return { path: 'https://unpkg.com/tiny-test-pkg@1.0.0/index.js'}
+          return { path: 'https://unpkg.com/tiny-test-pkg@1.0.0/index.js', namespace: 'a'}
         }
       });
       build.onLoad({ filter: /.*/ }, async (args: any) => {
