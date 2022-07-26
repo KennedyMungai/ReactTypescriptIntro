e562cfc7ee0963010c4308a71cc50726a910852c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-07-26 12:38:06 +0300

    Added a conditional check for the args.path values

--- jbook/src/plugins/unpkg-path-plugin.ts
+++ jbook/src/plugins/unpkg-path-plugin.ts
@@ -6,7 +6,13 @@ export const unpkgPathPlugin = () => {
     setup(build: esbuild.PluginBuild) {
       build.onResolve({ filter: /.*/ }, async (args: any) => {
         console.log('onResolve', args);
-        return { path: args.path, namespace: 'a' };
+
+        if (args.path === 'index.js') {
+          return { path: args.path, namespace: 'a' };
+        } else if (args.path === 'tiny-test-pkg')
+        {
+          return { path: 'https://unpkg.com/tiny-test-pkg@1.0.0/index.js'}
+        }
       });
       build.onLoad({ filter: /.*/ }, async (args: any) => {
         console.log('onLoad', args);
