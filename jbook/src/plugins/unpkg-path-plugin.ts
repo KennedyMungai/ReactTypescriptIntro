7def3b61a4deee32f1559be572f7bcc9c30a42b8
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-07-27 11:54:08 +0300

    Removed the build.onLoad code and its accompanying imports

--- jbook/src/plugins/unpkg-path-plugin.ts
+++ jbook/src/plugins/unpkg-path-plugin.ts
@@ -1,10 +1,6 @@
 import * as esbuild from 'esbuild-wasm';
 import axios from 'axios';
-import localforage from 'localforage';
 
-const fileCache = localforage.createInstance({
-  name: 'filecache'
-});
 
 export const unpkgPathPlugin = (inputCode: string) => {
   return {
@@ -32,35 +28,7 @@ export const unpkgPathPlugin = (inputCode: string) => {
           path: `https://unpkg.com/${args.path}`
         };
       });
-      build.onLoad({ filter: /.*/ }, async (args: any) => {
-        if (args.path === 'index.js') {
-          return {
-            loader: 'jsx',
-            contents: inputCode,
-          };
-        }
-
-        // Check to see if we have already fetched this file and if it is already in the cache
-        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
-
-        // if it is, return it immediately
-        if (cachedResult) {
-          return cachedResult;
-        }
-        
-        const { data, request } = await axios.get(args.path);
-        
-        const result: esbuild.OnLoadResult = {
-          loader: 'jsx',
-          contents: data,
-          resolveDir: new URL('./', request.responseURL).pathname
-        };
-
-        // Store the response in cache
-        await fileCache.setItem(args.path, result);
-
-        return result;
-      });
+      
     },
   };
 };
