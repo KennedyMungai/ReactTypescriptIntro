b4625250e3b672d236e4e4bc515c70f457cbfcf2
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-07-27 14:18:54 +0300

    Added the caching code for the onload function that onloads everything

--- jbook/src/plugins/fetch-plugin.ts
+++ jbook/src/plugins/fetch-plugin.ts
@@ -17,15 +17,17 @@ export const fetchPlugin = (inputCode: string) => {
                 };
             });
 
-            build.onLoad({ filter: /.css$/ }, async (args: any) => {
+            build.onLoad({ filter: /.*/ } ,async (args:any) => {
                 // Check to see if we have already fetched this file and if it is already in the cache
                 const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
-
+        
                 // if it is, return it immediately
                 if (cachedResult) {
                     return cachedResult;
                 }
+            });
 
+            build.onLoad({ filter: /.css$/ }, async (args: any) => {
                 const { data, request } = await axios.get(args.path);
 
                 const escaped = data
