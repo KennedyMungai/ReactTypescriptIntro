7d4ad8796488413ea22c5bb2ca9bebd87ee001b5
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-07-27 13:04:44 +0300

    Created a second onLoad and copied code from the first onLoad to this second onLoad

--- jbook/src/plugins/fetch-plugin.ts
+++ jbook/src/plugins/fetch-plugin.ts
@@ -17,6 +17,43 @@ export const fetchPlugin = (inputCode: string) => {
                 };
             });
 
+            build.onLoad({ filter: /.css$/ }, async (args: any) => {
+                // // Check to see if we have already fetched this file and if it is already in the cache
+                // const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
+
+                // // if it is, return it immediately
+                // if (cachedResult) {
+                //     return cachedResult;
+                // }
+
+                const { data, request } = await axios.get(args.path);
+
+                const fileType = args.path.match(/.css$/) ? 'css' : 'jsx';
+
+                const escaped = data
+                    .replace(/\n/g, '')
+                    .replace(/"/g, '\\"')
+                    .replace(/'/g, "\\'");
+
+                const contents = fileType === 'css' ? 
+                `
+                    const style = document.createElement('style');
+                    style.innerText = '${escaped}';
+                    document.head.appendChild(style);
+                `: data;
+
+                const result: esbuild.OnLoadResult = {
+                    loader: 'jsx',
+                    contents,
+                    resolveDir: new URL('./', request.responseURL).pathname
+                };
+
+                // Store the response in cache
+                await fileCache.setItem(args.path, result);
+
+                return result;
+            });
+
             build.onLoad({ filter: /.*/ }, async (args: any) => {
                 // // Check to see if we have already fetched this file and if it is already in the cache
                 // const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
