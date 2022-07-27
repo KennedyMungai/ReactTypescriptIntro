b5ab523ce31bf0b79e9c24461d834d505461f79a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-07-27 13:05:56 +0300

    Uncommented the caching code

--- jbook/src/plugins/fetch-plugin.ts
+++ jbook/src/plugins/fetch-plugin.ts
@@ -18,13 +18,13 @@ export const fetchPlugin = (inputCode: string) => {
             });
 
             build.onLoad({ filter: /.css$/ }, async (args: any) => {
-                // // Check to see if we have already fetched this file and if it is already in the cache
-                // const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
+                // Check to see if we have already fetched this file and if it is already in the cache
+                const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
 
-                // // if it is, return it immediately
-                // if (cachedResult) {
-                //     return cachedResult;
-                // }
+                // if it is, return it immediately
+                if (cachedResult) {
+                    return cachedResult;
+                }
 
                 const { data, request } = await axios.get(args.path);
 
