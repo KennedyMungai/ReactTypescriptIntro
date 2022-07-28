a34932688db55e291124d0affd576e59ef11f147
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-07-28 10:29:50 +0300

    Added the content window method of current

--- jbook/src/index.tsx
+++ jbook/src/index.tsx
@@ -42,7 +42,7 @@ const App = () => {
 
         // setCode(result.outputFiles[0].text);
 
-        iframe.current.postMessage(result.outputFiles[0].text, '*');
+        iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
     };
 
     const html = `
