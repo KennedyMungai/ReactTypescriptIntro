cd122387c999cec0719163c21151fa4adb515c4f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-07-28 10:32:04 +0300

    Changed out the console.log for event.data with the eval() for event.data

--- jbook/src/index.tsx
+++ jbook/src/index.tsx
@@ -53,7 +53,7 @@ const App = () => {
 
                 <script>
                     window.addEventListener('message', (event) => {
-                        console.log(event.data);
+                        eval(event.data);
                     }, false);
                 </script>
             </body>
