e1c34ebc1ca81c30a03e07dbccd6df4fc533b9dc
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-07-28 10:28:36 +0300

    Commented out the setCode method and added the postMessage method from iframe.current which outputs result.outputFiles[0].text

--- jbook/src/index.tsx
+++ jbook/src/index.tsx
@@ -40,7 +40,9 @@ const App = () => {
             }
         });
 
-        setCode(result.outputFiles[0].text);
+        // setCode(result.outputFiles[0].text);
+
+        iframe.current.postMessage(result.outputFiles[0].text, '*');
     };
 
     const html = `
