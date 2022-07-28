0cff562ae3d06b38d21aa8054fad67bcba11806b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-07-28 21:18:49 +0300

    Added the codeshift import statement which takes it from jscodeshift

--- jbook/src/components/code-editor.tsx
+++ jbook/src/components/code-editor.tsx
@@ -3,6 +3,7 @@ import MonacoEditor, { EditorDidMount } from '@monaco-editor/react';
 import prettier from 'prettier';
 import parser from 'prettier/parser-babel';
 import { useRef } from 'react';
+import codeShift from 'jscodeshift';
 
 interface CodeEditorProps {
     initialValue: string;
