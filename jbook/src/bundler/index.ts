b18d5c30c84a0b61f4aa847bc03f9a2582dbe857
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-07-29 10:16:12 +0300

    Imported the unpkgPathPlugin for index.ts

--- jbook/src/bundler/index.ts
+++ jbook/src/bundler/index.ts
@@ -1,4 +1,6 @@
 import * as esbuild from 'esbuild-wasm';
+import { fetchPlugin } from '../plugins/fetch-plugin';
+import { unpkgPathPlugin } from '../plugins/unpkg-path-plugin';
 
 export default (rawCode: string) => {
 
