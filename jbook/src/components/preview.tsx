5f364160ced03293e3e2b7c87eae2943f4f97e2c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-07-29 08:49:20 +0300

    Addd the HTML snippet

--- jbook/src/components/preview.tsx
+++ jbook/src/components/preview.tsx
@@ -2,8 +2,32 @@ interface PreviewProps {
     code: string;
 }
 
-const Preview: React.FC<PreviewProps> = ({ code }) => {
 
+const html = `
+<html>
+    <head></head>
+    <body>
+        <div id="root"></div>
+
+        <script>
+            window.addEventListener('message', (event) => {
+                try{
+                    eval(event.data);
+                } catch (error) {
+                    const root = document.querySelector('#root');
+                    root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + error + '</div>'
+                    console.error(error);
+                }
+            }, false);
+        </script>
+    </body>
+</html>
+`;
+
+const Preview: React.FC<PreviewProps> = ({ code }) => {
+    return {
+        < iframe />
+    };
 };
 
 export default Preview;
