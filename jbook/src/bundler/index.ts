36b643d3e5f2ec1fc02c4c8bc3be50394e13025b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-07-29 15:27:16 +0300

    Added the const result inside a try - catch statement

--- jbook/src/bundler/index.ts
+++ jbook/src/bundler/index.ts
@@ -12,19 +12,23 @@ const bundle = async (rawCode: string) => {
         });
     }
 
-    const result = await service.build({
-        entryPoints: ['index.js'],
-        bundle: true,
-        write: false,
-        plugins: [
-            unpkgPathPlugin(),
-            fetchPlugin(rawCode)
-        ],
-        define: {
-            'process.env.NODE_ENV': '"production"',
-            global: 'window'
-        }
-    });
+    try {
+        const result = await service.build({
+            entryPoints: ['index.js'],
+            bundle: true,
+            write: false,
+            plugins: [
+                unpkgPathPlugin(),
+                fetchPlugin(rawCode)
+            ],
+            define: {
+                'process.env.NODE_ENV': '"production"',
+                global: 'window'
+            }
+        });
+    } catch (error) {
+
+    }
 
     return result.outputFiles[0].text;
 };
