efc3f26b500e6a50edd4bcf2df3a4119f566ac82
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-07-29 08:48:53 +0300

    Removed the html snippet

--- jbook/src/index.tsx
+++ jbook/src/index.tsx
@@ -48,26 +48,6 @@ const App = () => {
         iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
     };
 
-    const html = `
-        <html>
-            <head></head>
-            <body>
-                <div id="root"></div>
-
-                <script>
-                    window.addEventListener('message', (event) => {
-                        try{
-                            eval(event.data);
-                        } catch (error) {
-                            const root = document.querySelector('#root');
-                            root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + error + '</div>'
-                            console.error(error);
-                        }
-                    }, false);
-                </script>
-            </body>
-        </html>
-    `;
 
     return (
         <div>
