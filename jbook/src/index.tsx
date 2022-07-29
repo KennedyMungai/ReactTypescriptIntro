c99b65a5a9abbbe4056ce6283039f4651b6aad6b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-07-29 10:54:24 +0300

    Added an import statement for Code Cell

--- jbook/src/index.tsx
+++ jbook/src/index.tsx
@@ -1,5 +1,6 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import ReactDOM from 'react-dom';
+import CodeCell from './components/code-cell';
 
 const App = () => {
     const [code, setCode] = useState('');
