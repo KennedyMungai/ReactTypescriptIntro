8c591f6a5dacaefef8471511be82c3935a485b9f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-07-29 12:42:19 +0300

    Moved all the props to the else condition of the resizable props orientation

--- jbook/src/components/resizable.tsx
+++ jbook/src/components/resizable.tsx
@@ -14,18 +14,16 @@ const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
         };
     } else {
         resizableProps = {
-
+            minConstraints={ [Infinity, window.innerHeight * 0.2]}
+            maxConstraints={ [Infinity, window.innerHeight * 0.9]}
+            height={ 300}
+            width={ Infinity }
+            resizeHandles={ ['s']}
         };
     }
 
     return (
-        <ResizableBox
-            minConstraints={[Infinity, window.innerHeight * 0.2]}
-            maxConstraints={[Infinity, window.innerHeight * 0.9]}
-            height={300}
-            width={Infinity}
-            resizeHandles={['s']}
-        >
+        <ResizableBox>
             {children}
         </ResizableBox>
     );
