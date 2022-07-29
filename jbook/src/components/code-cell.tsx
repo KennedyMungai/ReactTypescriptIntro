4d2e552965c10ec033c3f0b279acfb3ba23ad5a3
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-07-29 11:26:46 +0300

    Added thr CodeEditor div in the resizable react component

--- jbook/src/components/code-cell.tsx
+++ jbook/src/components/code-cell.tsx
@@ -14,16 +14,19 @@ const CodeCell = () => {
     };
 
     return (
-        <div>
-            <CodeEditor
-                initialValue='const a = 1;'
-                onChange={(value) => setInput(value)}
-            />
+        <Resizable>
             <div>
-                <button onClick={onClick}>Submit</button>
+                <CodeEditor
+                    initialValue='const a = 1;'
+                    onChange={(value) => setInput(value)}
+                />
+                <div>
+                    <button onClick={onClick}>Submit</button>
+                </div>
+                <Preview code={code} />
             </div>
-            <Preview code={code} />
-        </div>
+        </Resizable>
+
     )
 };
 
