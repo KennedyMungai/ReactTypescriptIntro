09de6887dc5cd623e2b160abf8bba7de3db933ae
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-07-29 14:48:45 +0300

    Using the useEffect hook from react

--- jbook/src/components/code-cell.tsx
+++ jbook/src/components/code-cell.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import CodeEditor from './code-editor';
 import Preview from './preview'
 import bundle from '../bundler';
