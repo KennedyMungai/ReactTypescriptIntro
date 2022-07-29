213daf336cb6f93adea4d4234c0c2c64f0b52e5c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-07-29 12:26:50 +0300

    Added the min constrains for the resizable elements

--- jbook/src/components/resizable.tsx
+++ jbook/src/components/resizable.tsx
@@ -8,6 +8,7 @@ interface ResizableProps {
 const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
     return (
         <ResizableBox
+            minConstraints={[Infinity, window.innerHeight * 0.2]}
             maxConstraints={[Infinity, window.innerHeight * 0.9]}
             height={300}
             width={Infinity}
