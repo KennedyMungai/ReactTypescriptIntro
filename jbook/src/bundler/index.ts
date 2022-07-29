5e8bf44e5bdea04f161388503603b2606e953319
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-07-29 10:18:28 +0300

    Removed the esbuild.StartService

--- jbook/src/bundler/index.ts
+++ jbook/src/bundler/index.ts
@@ -3,5 +3,8 @@ import { fetchPlugin } from '../plugins/fetch-plugin';
 import { unpkgPathPlugin } from '../plugins/unpkg-path-plugin';
 
 export default (rawCode: string) => {
-
+    await esbuild.startService({
+        worker: true,
+        wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm'
+    });
 }
