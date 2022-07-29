453cbf14926ada67b4cbd8e114238bf029b7d717
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-07-29 11:40:40 +0300

    Added a proper resize handle to teh Resizable Box react element

--- jbook/src/components/resizable.tsx
+++ jbook/src/components/resizable.tsx
@@ -10,7 +10,8 @@ const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
     return (
         <ResizableBox
             height={300}
-            width={300}>
+            width={300}
+            resizeHandles={['s']}>
             {children}
         </ResizableBox>);
 };
