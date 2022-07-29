f2aded99329f20e3b4f69234e8e3e0ecb8f2b0db
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-07-29 10:19:10 +0300

    Made teh default function asynchronous

--- jbook/src/bundler/index.ts
+++ jbook/src/bundler/index.ts
@@ -2,7 +2,7 @@ import * as esbuild from 'esbuild-wasm';
 import { fetchPlugin } from '../plugins/fetch-plugin';
 import { unpkgPathPlugin } from '../plugins/unpkg-path-plugin';
 
-export default (rawCode: string) => {
+export default async (rawCode: string) => {
     await esbuild.startService({
         worker: true,
         wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm'
