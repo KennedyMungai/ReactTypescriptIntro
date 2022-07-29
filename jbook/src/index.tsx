770c4920ff1250826fc7e715e43b98442fc50e81
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-07-29 10:56:20 +0300

    Removed the CodeEditor code as it is being handled by the code-cell component

--- jbook/src/index.tsx
+++ jbook/src/index.tsx
@@ -5,14 +5,7 @@ import CodeCell from './components/code-cell';
 const App = () => {
     return (
         <div>
-            <CodeEditor
-                initialValue='const a = 1;'
-                onChange={(value) => setInput(value)}
-            />
-            <div>
-                <button onClick={onClick}>Submit</button>
-            </div>
-            <Preview code={code} />
+
         </div>
     )
 };
