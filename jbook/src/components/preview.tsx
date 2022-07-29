26bb2dd15895165abdc079e604193b114ef15ea0
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-07-29 15:15:03 +0300

    Moved all the code in the catch statement to the handle error function

--- jbook/src/components/preview.tsx
+++ jbook/src/components/preview.tsx
@@ -15,16 +15,16 @@ const html = `
 
         <script>
             const handleError = (error) => {
-                
+                const root = document.querySelector('#root');
+                    root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + error + '</div>'
+                    console.error(error);
             };
 
             window.addEventListener('message', (event) => {
                 try{
                     eval(event.data);
                 } catch (error) {
-                    const root = document.querySelector('#root');
-                    root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + error + '</div>'
-                    console.error(error);
+                    
                 }
             }, false);
         </script>
