734d4146d06a59eeb008e44baef543ca9b67e201
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-07-30 11:56:37 +0300

    Added the appropriate types to the respective interfaces

--- jbook/src/state/actions/index.ts
+++ jbook/src/state/actions/index.ts
@@ -1,17 +1,18 @@
+import { Action } from "redux";
 import { ActionType } from "../action-types";
 
 interface MoveCellAction {
-
+    type: ActionType.MOVE_CELL;
 }
 
 interface DeleteCellAction {
-
+    type: ActionType.DELETE_CELL;
 }
 
 interface InsertCellAction {
-
+    type: ActionType.INSERT_CELL_BEFORE;
 }
 
 interface UpdateCellAction {
-
+    type: ActionType.UPDATE_CELL;
 }
