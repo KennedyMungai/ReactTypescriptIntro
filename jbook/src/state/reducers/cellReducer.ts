5b63654304ca20a757157231e41c2201da5dbc20
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-07-30 12:33:01 +0300

    Created a CellState interface and inported the Cell interface from Cell.ts

--- jbook/src/state/reducers/cellReducer.ts
+++ jbook/src/state/reducers/cellReducer.ts
@@ -1,6 +1,12 @@
 import { ActionType } from '../action-types';
 import { Action } from '../actions';
+import { Cell } from '../cell';
 
 interface CellsState {
-
+    loading: boolean;
+    error: string | null;
+    order: string[],
+    data: {
+        [key: string]: Cell
+    }
 }
