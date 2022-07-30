52e5c147bf5a024f862227d0fdec03ccd1f1672f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-07-30 09:38:28 +0300

    Added some extra UI logic for the text editor

--- jbook/src/components/text-editor.tsx
+++ jbook/src/components/text-editor.tsx
@@ -2,7 +2,7 @@ import MDEditor from "@uiw/react-md-editor";
 
 const TextEditor: React.FC = () => {
     return <div>
-        <MDEditor />
+        <MDEditor.Markdown source={"# Header"} />
     </div>
 };
 
