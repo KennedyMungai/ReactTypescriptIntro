4c49eebc60abc1e49fd1e4f10624a00dcb2f09c2
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-07-30 09:32:56 +0300

    Added an import statement for TextEditor

--- jbook/src/index.tsx
+++ jbook/src/index.tsx
@@ -1,6 +1,7 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import ReactDOM from 'react-dom';
 import CodeCell from './components/code-cell';
+import TextEditor from './components/text-editor';
 
 const App = () => {
     return (
