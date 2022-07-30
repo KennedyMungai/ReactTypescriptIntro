1bb4f7070403d8d56547c6d2b6ea68a624c78dee
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-07-30 09:41:06 +0300

    Added a new state manager called editing

--- jbook/src/components/text-editor.tsx
+++ jbook/src/components/text-editor.tsx
@@ -2,6 +2,8 @@ import MDEditor from "@uiw/react-md-editor";
 import { useState } from "react";
 
 const TextEditor: React.FC = () => {
+    const [editing, setEditing] = useState(false);
+
     return <div>
         <MDEditor.Markdown source={"# Header"} />
     </div>
