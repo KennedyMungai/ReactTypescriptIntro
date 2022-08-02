e3e437f40ed78a9c4c2a03664a3d85fbb5e8848b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-02 15:47:21 +0300

    Removed the return type for the reducer function as the immer produce function makes the funvtion return type variable. It might return null

--- jbook/src/state/reducers/cellReducer.ts
+++ jbook/src/state/reducers/cellReducer.ts
@@ -19,7 +19,7 @@ const initialState: CellsState = {
     data: {}
 }
 
-const reducer = produce((state: CellsState = initialState, action: Action): CellsState => {
+const reducer = produce((state: CellsState = initialState, action: Action) => {
     switch (action.type) {
         case ActionType.UPDATE_CELL:
             const {id, content} = action.payload;
