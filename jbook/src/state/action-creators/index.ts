bfc156735ead16cf36e5b7be87954a24bc928733
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-02 15:02:58 +0300

    Created the return statement with the appropriate return type

--- jbook/src/state/action-creators/index.ts
+++ jbook/src/state/action-creators/index.ts
@@ -8,7 +8,16 @@ import {
         } from '../actions';
 
 
-export const updateCell = (id: string, content: string): UpdateCellAction => {};
+export const updateCell = (id: string, content: string): UpdateCellAction => {
+
+    return {
+        type: ActionType.UPDATE_CELL,
+        payload: {
+            id, 
+            content
+        }
+    };
+};
 
 export const deleteCell = (): DeleteCellAction => {};
 
