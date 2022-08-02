5c9dd2912d4dcbfe5470782d4f7d6225e123e42d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-02 14:40:15 +0300

    Added a store for the import from the state

--- jbook/src/index.tsx
+++ jbook/src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import CodeCell from './components/code-cell';
 import TextEditor from './components/text-editor';
+import { store } from './state';
 
 const App = () => {
     return (
