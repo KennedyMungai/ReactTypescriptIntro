c97764ff9337817161bd34e37284011cff29d04f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-03 08:33:08 +0300

    Replaced Text Editor with Cell List and refactored the code

--- jbook/src/index.tsx
+++ jbook/src/index.tsx
@@ -1,15 +1,15 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import CellList from './components/cell-list';
 import CodeCell from './components/code-cell';
-import TextEditor from './components/text-editor';
 import { store } from './state';
 
 const App = () => {
     return (
         <Provider store={store}>
             <div>
-                <TextEditor />
+                <CellList />
             </div>
         </Provider>
     );
