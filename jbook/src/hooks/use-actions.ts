309276159249f4af9fffa02232d45d05a8e5acef
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-03 09:27:03 +0300

    Changed the spellin from useAction to useActions and initiated teh const updateCell to useActions

--- jbook/src/hooks/use-actions.ts
+++ jbook/src/hooks/use-actions.ts
@@ -2,8 +2,10 @@ import { useDispatch } from "react-redux";
 import { bindActionCreators } from "redux";
 import { actionCreators } from '../state';
 
-export const useAction = () => {
+export const useActions = () => {
     const dispatch = useDispatch();
 
     return bindActionCreators(actionCreators, dispatch);
 };
+
+const { updateCell } = useActions();
