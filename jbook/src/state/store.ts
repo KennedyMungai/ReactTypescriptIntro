e365ccd5a3786c41e695ec938867954624473719
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-03 08:18:50 +0300

    Added some dispatch code

--- jbook/src/state/store.ts
+++ jbook/src/state/store.ts
@@ -4,3 +4,11 @@ import { ActionType } from "./action-types";
 import reducers from "./reducers";
 
 export const store = createStore(reducers, {}, applyMiddleware(thunk));
+
+store.dispatch({
+    type: ActionType.INSERT_CELL_BEFORE,
+    payload: {
+        id: null,
+        type: 'code'
+    }
+})
