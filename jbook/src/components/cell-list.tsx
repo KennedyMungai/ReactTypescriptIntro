49d154cb39e5953edceb93040e815aa989db147c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-03 08:53:41 +0300

    Returned a map of the order data structure

--- jbook/src/components/cell-list.tsx
+++ jbook/src/components/cell-list.tsx
@@ -3,8 +3,10 @@ import { useTypedSelector } from "../hooks/use-typed-selector";
 useTypedSelector
 
 const CellList: React.FC = () => {
-    useTypedSelector((cells: {order, data}) => {
-
+    useTypedSelector(({cells: {order, data}}) => {
+        return order.map(() => {
+            
+        })
     });
 
     return <div>Cell List</div>;
