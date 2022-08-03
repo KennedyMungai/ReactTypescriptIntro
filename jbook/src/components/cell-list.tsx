31b02a9e034fd74474afb6212a7094757e56da31
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-03 08:58:47 +0300

    Condensed the useTypedSelector to a line

--- jbook/src/components/cell-list.tsx
+++ jbook/src/components/cell-list.tsx
@@ -3,11 +3,7 @@ import { useTypedSelector } from "../hooks/use-typed-selector";
 useTypedSelector
 
 const CellList: React.FC = () => {
-    useTypedSelector(({cells: {order, data}}) => 
-        order.map((id) =>
-            data[id]
-        )
-    );
+    useTypedSelector(({ cells: { order, data } }) => order.map((id) => data[id] ));
 
     return <div>Cell List</div>;
 };
