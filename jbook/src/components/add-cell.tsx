81f953146ffaa3d4cc1b044bdbfc2a738fe7b151
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-04 15:38:54 +0300

    Added the AddCellProps interface to the file

--- jbook/src/components/add-cell.tsx
+++ jbook/src/components/add-cell.tsx
@@ -1,5 +1,9 @@
 import './add-cell.css';
-import React from 'react'
+import React from 'react';
+
+interface AddCellProps {
+    nextCellId: string;
+};
 
 const AddCell: React.FC = () => {
   return (
