cef221aa6a8b772415cce59beff1936ecbc201d8
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-04 15:01:45 +0300

    Replaced the font awesomne trash icon with the X button

--- jbook/src/components/action-bar.tsx
+++ jbook/src/components/action-bar.tsx
@@ -24,7 +24,7 @@ const ActionBar: React.FC<ActionBarProps> = ( {id} ) => {
 
         <button className='button is-primary is-small' onClick={() => deleteCell(id)}>
           <span className="icon">
-            <i className="fa fa-trash" aria-hidden="true"></i>
+            <i className="fas fa-times" aria-hidden="true"></i>
           </span>
         </button>
     </div>
