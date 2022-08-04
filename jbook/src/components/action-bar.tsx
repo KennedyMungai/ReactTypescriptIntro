75331463c2c5266d8b7b6b0169b7a8c06b0f1bd5
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-04 14:24:28 +0300

    Added some buttons to the action-bar component

--- jbook/src/components/action-bar.tsx
+++ jbook/src/components/action-bar.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const ActionBar: React.FC = () => {
   return (
-    <div>ActionBar</div>
+    <div>
+        <button>Up</button>
+        <button>Down</button>
+        <button>Delete</button>
+    </div>
   )
 }
 
